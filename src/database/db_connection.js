db4eddb20b9a54bfb4e4f7afc024bb6ade7ebef4
pbywater <philippabywater@gmail.com>
2017-08-04 14:49:39 +0100

    commiting answer to demonstrate working solution that exists in master in a solutions folder

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -1,20 +1,24 @@
-const { Pool } = require('pg');
+// Add code below to connect to your database
+const {
+    Pool
+} = require('pg');
 const url = require('url');
 require('env2')('./config.env');
 
-if(!process.env.DB_URL) throw new Error('Enviroment variable DB_URL must be set');
+if (!process.env.DB_URL) throw new Error('Enviroment variable DB_URL must be set');
 
 const params = url.parse(process.env.DB_URL);
 const [username, password] = params.auth.split(':');
 
 const options = {
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  max: process.env.DB_MAX_CONNECTIONS || 2,
-  user: username,
-  password: password,
-  ssl: (options.host !== 'localhost'),
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    max: process.env.DB_MAX_CONNECTIONS || 2,
+    user: username,
+    password: password,
 };
+
+options.ssl = (options.host !== 'localhost');
 
 module.exports = new Pool(options);
